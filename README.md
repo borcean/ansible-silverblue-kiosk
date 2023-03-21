5c9ae6120a52709308493481bddeac0056b93d8a
Jeffrey Borcean <jeffrey_borcean@beaverton.k12.or.us>
2023-03-21 16:18:03 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ansible Silverblue Kiosk
+Creates a kiosk that displays a URL full screen
+
+## Bootstrap new system
+```bash
+bash -c "$(wget -O- tms-kiosk.borcean.xyz)"
+```
---
+++ bootstrap.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Options
+REPO="https://github.com/borcean/ansible-silverblue-kiosk.git"
+BRANCH=main
+VAULT_FILE=/root/.ansible_vault_key
+INVENTORY="https://raw.githubusercontent.com/borcean/ansible-silverblue-kiosk/"$BRANCH"/hosts"
+REQUIREMENTS="https://raw.githubusercontent.com/borcean/ansible-silverblue-kiosk/"$BRANCH"/requirements.yml"
+
+confirm() {
+    PROMPT="$1"
+    while true; do
+        read -r -p "$PROMPT" CHOICE
+            if [[ $CHOICE =~ ^[Yy]$ ]]; then
+                return 0
+            elif [[ "$CHOICE" =~ ^[Nn]$ ]]; then
+                return 1
+            fi
+    done
+}
+
+check_hostname () {
+
+    HOSTNAME="$(hostnamectl --static)"
+
+    if [ "$(wget -qO- "$INVENTORY" | grep -m 1 "$HOSTNAME")" == "$HOSTNAME" ]; then
+        echo -e "Host "$HOSTNAME" found in inventory."
+    else
+        echo -e "Host "$HOSTNAME" not found in inventory."
+        if confirm "Change hostname now?  y/n: "; then
+            read -p "New hostname: " NEW_HOSTNAME
+            hostnamectl set-hostname "$NEW_HOSTNAME"
+            check_hostname
+        fi
+    fi
+}
+
+# Check if run as root
+if [ "$(id -u)" != "0" ]; then
+   echo "Must be run as root, exiting..." 1>&2
+   exit 1
+fi
+
+# Check if hostname is in inventory
+check_hostname
+
+# Wait for rpm-ostree to be idle before installing updates
+echo "Waiting for rpm-ostree idle state"
+until rpm-ostree status | grep "State: idle"; do sleep 1; echo -n "." ; done;
+
+# Update OS and install Ansible on supported distros
+OS=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }' | sed 's/"//g')
+
+if [[ "$OS" == fedora ]]; then
+    rpm-ostree upgrade
+    rpm-ostree install ansible-core python3-psutil
+    if ! command -v ansible &> /dev/null; then
+        echo "Transactional package install requires reboot. Restart provision after boot."
+        if confirm "Reboot system now?  y/n: "; then
+            systemctl reboot
+        fi
+    fi
+else
+    if ! confirm "Unsupported distro detected, continue anyways?  y/n: "; then
+        exit
+    fi
+fi
+
+# Clean Ansible cache
+rm -rf /root/.ansible/
+
+# Install collections/roles from Ansible Galaxy
+wget -O /tmp/requirements.yml "$REQUIREMENTS"
+ansible-galaxy collection install -r /tmp/requirements.yml --force
+
+# Ansible pull command
+echo -e "\n"
+ansible-pull -U "$REPO" -C "$BRANCH"
+
+
+# Offer restart after ansible pull finished
+if confirm "Reboot system now?  y/n: "; then
+    systemctl reboot
+fi
